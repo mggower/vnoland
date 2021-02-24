84e31a8efdc6986d94d03bd6d4c68cf86b3cc286
mggower <gowermikey@gmail.com>
2021-02-24 15:35:06 -0500

    refactor

---
+++ deps.ts
@@ -0,0 +1,9 @@
+export { dirname, join } from "https://deno.land/std@0.74.0/path/mod.ts";
+export * as log from "https://deno.land/std@0.74.0/log/mod.ts";
+export {
+  Application,
+  Router,
+  send,
+} from "https://deno.land/x/oak@v6.5.0/mod.ts";
+export { config } from "https://deno.land/x/dotenv/mod.ts";
+
---
+++ server.ts
@@ -0,0 +1,44 @@
+import { Application, join, send } from "./deps.ts";
+
+// const port: any = Deno.env.get("PORT") || 3000;
+const port = 8080;
+const hostname = "0.0.0.0";
+
+const server: Application = new Application();
+
+server.use(async (ctx, next) => {
+  const filePath = ctx.request.url.pathname;
+  if (filePath.slice(0, 7) === "/assets") {
+    await send(ctx, filePath, {
+      root: join(Deno.cwd(), "src"),
+    });
+  } else if (filePath === "/") {
+    await send(ctx, filePath, {
+      root: join(Deno.cwd(), "public"),
+      index: "index.html",
+    });
+  } else if (filePath === "/build.js") {
+    ctx.response.type = "application/javascript";
+    await send(ctx, filePath, {
+      root: join(Deno.cwd(), "vno-build"),
+      index: "build.js",
+    });
+  } else if (filePath === "/style.css") {
+    ctx.response.type = "text/css";
+    await send(ctx, filePath, {
+      root: join(Deno.cwd(), "vno-build"),
+      index: "style.css",
+    });
+  } else await next();
+});
+
+server.addEventListener("error", (err) => console.warn(err));
+server.addEventListener("listen", () => {
+  console.log(`server is listening on ${hostname}:${port}`);
+});
+
+if (import.meta.main) {
+  await server.listen({ port, hostname });
+}
+
+export { server };
